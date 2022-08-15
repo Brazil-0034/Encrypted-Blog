c24d970d0a08b3919f202611e4c26423a1037475
Austin <66288732+Brazil-0034@users.noreply.github.com>
2022-08-15 04:55:35 -0700

    Better Sorting + Easy Decryption

--- static/script.js
+++ static/script.js
@@ -1,3 +1,9 @@
+// if browser is using HTTP, add a link to the https version
+if (window.location.protocol == "http:") {
+    var httpsLink = window.location.href.replace("http:", "https:");
+    document.body.innerHTML = '<a href="' + httpsLink + '">Click here to access this page using HTTPS</a>';
+    document.querySelector("a").className = "redir-link";
+}
 
 function autoscale() {
     var textarea = document.getElementById("content");
@@ -21,7 +27,6 @@ function newPost() {
 
 function sendPost() {
     var postURL = window.location.href + "post/";
-    console.log(postURL);
     // create url args
     const args = new URLSearchParams();
 
@@ -41,7 +46,6 @@ function httpRequest(url, method, callback) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
             callback(this);
         }
     };
@@ -51,6 +55,7 @@ function httpRequest(url, method, callback) {
 
 // get the posts from the server
 function getPosts() {
+    const postbox = document.querySelector(".postbox");
     httpRequest("/getposts", "GET", function (xhttp) {
         var posts = xhttp.responseText;
         // split by line break
@@ -63,7 +68,7 @@ function getPosts() {
             let post = document.createElement("div");
             post.className = "post";
             post.innerHTML = "<h1>" + postData.title.substring(8) + "</h1><br><p>" + postData.content + "</p><br>";
-            document.querySelector(".postbox").appendChild(post);
+            postbox.append(post);
 
             // create a "decrypt" button on each post
             let decryptButton = document.createElement("a");
@@ -94,7 +99,67 @@ function getPosts() {
             }
             post.parentElement.appendChild(decryptButton);
         }
+        for (let i = 1; i < postbox.childNodes.length; i++) postbox.insertBefore(postbox.childNodes[i], postbox.firstChild);
     });
 }
 
 getPosts();
+
+// if the user clicks the "decrypt-all-button" button, decrypt all posts
+document.querySelector("#decrypt-all-button").onclick = function () {
+    const univPasskey = prompt("Enter the passkey");
+    // basically the getposts function, but everything gets decrypted
+    // start my clearing the .postbox element
+    const postbox = document.querySelector(".postbox");
+    postbox.innerHTML = "";
+
+    httpRequest("/getposts", "GET", function (xhttp) {
+        var posts = xhttp.responseText;
+        // split by line break
+        posts = posts.split("\n");
+        posts.pop();
+
+        let i = 0;
+        function decryptPost(i)
+        {
+            let postData = JSON.parse(posts[i]);
+            let post = document.createElement("div");
+            post.className = "post decrypted";
+
+            // decrypt the title and content
+            let title = postData.title;
+            let content = postData.content;
+
+            // url params
+            const args = new URLSearchParams();
+            args.append("passkey", univPasskey);
+            args.append("content", title);
+            
+            // make request
+            httpRequest(window.location.href + "decrypt?" + args.toString(), "GET", function (xhttp) {
+                let decryptedContent = xhttp.responseText;
+                post.innerHTML = "<h1>" + decryptedContent + "</h1><br><p>"
+
+                args.set("content", content);
+                httpRequest(window.location.href + "decrypt?" + args.toString(), "GET", function (xhttp) {
+                    let decryptedContent = xhttp.responseText;
+                    post.innerHTML += decryptedContent + "</p><br>";
+                    console.log(decryptedContent);
+                    postbox.append(post);
+                    
+                    i++;
+                    if (i < posts.length)
+                    {
+                        decryptPost(i);
+                    }
+                    else
+                    {
+                        for (let x = 1; x < postbox.childNodes.length; x++) postbox.insertBefore(postbox.childNodes[x], postbox.firstChild);
+                    }
+                });
+            });
+        }
+
+        decryptPost(i);
+    });
+}
